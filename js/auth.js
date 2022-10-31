032ff0ce56011af8be2a764c68ddd68f3cf389d2
jladd00123 <justin@justinmichaelladd.com>
2022-10-31 11:27:43 -0700

    fix login

--- js/auth.js
+++ js/auth.js
@@ -1,21 +1,22 @@
 
 var AUTH_NAME = 'authenticated';
+var CONTEXT_PATH = '/eoc2023.github.io';
 var USERNAME = 'admin';
 var PASSWORD = 'admin';
 
-if (!isAuthenticated() && document.location.pathname !== '/login.html') {
-    document.location.href = '/login.html';
+if (!isAuthenticated() && document.location.pathname !== (CONTEXT_PATH + '/login.html')) {
+    document.location.href = (CONTEXT_PATH + '/login.html');
 }
 
 function isAuthenticated() {
-    return true;
-    //return sessionStorage[AUTH_NAME];
+    //return true;
+    return sessionStorage[AUTH_NAME];
 }
 
 function authenticate(username, password) {
     if (username === USERNAME && password === PASSWORD) {
         sessionStorage[AUTH_NAME] = true;
-        document.location.href = '/';
+        document.location.href = CONTEXT_PATH;
         return true;
     } 
     return false;
